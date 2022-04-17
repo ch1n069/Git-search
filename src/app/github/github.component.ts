b1f87c2a821418089ae6d7f89d71037b1ee6c9b8
Moringa School <brunolalpachino@gmail.com>
2022-04-17 20:57:15 +0300

    declared the fields in our component ts file

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubComponent implements OnInit {
 
+  public userQuerry:string;
+  public gitProfile:any;
+  public gitRepo:any[];
+  
   constructor() { }
 
   ngOnInit(): void {
